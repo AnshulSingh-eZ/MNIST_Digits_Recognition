efa36aa179ef44bb6f19e32d9638c5429db2ae64
AnshulSingh-eZ <anshul06.singh2013@gmail.com>
2026-01-05 14:23:45 +0530

    Generalized the model with CNN

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.optimizers import SGD
 
@@ -12,14 +12,17 @@ print(X_train.shape)
 X_train, X_test = X_train/255.0, X_test/255.0 
 
 model = Sequential([
-    Flatten(input_shape=(28,28)),
-    Dense(784, activation='relu'),
+    Conv2D(32, (2,2), activation='relu', input_shape=(28,28,1)),
+    MaxPooling2D((2,2)),
+    Conv2D(64, (2,2), activation='relu'),
+    MaxPooling2D(2,2),
+    Flatten(),
     Dense(128, activation='relu'),
     Dense(10, activation='softmax')
 ])
 optimizer = 'adam'
 model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-data = model.fit(X_train, Y_train, epochs=10)
+data = model.fit(X_train, Y_train, epochs=5)
 model.save("adam_digit_rec.keras")
 print("Model successfully saved!!")
 pd.DataFrame(data.history).to_csv("adam_digit_rec.csv", index=False)
--- test.py
+++ test.py
@@ -1,19 +1,14 @@
 import cv2
 from tensorflow.keras.models import load_model
 
-img = cv2.imread("1.jpeg", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("3.jpeg", cv2.IMREAD_GRAYSCALE)
 img = cv2.resize(img, (28,28))
 img = 255-img
 img = img/255.0
 img = img.reshape(1,28,28)
 
-print(img)
-
 model1 = load_model('adam_digit_rec.keras')
-model2 = load_model('sgd_1_digit_rec.keras')
 pred1 = model1.predict(img)
-pred2 = model2.predict(img)
 
 a = pred1.argmax()
-b = pred2.argmax()
-print(a, b)
+print(a)
